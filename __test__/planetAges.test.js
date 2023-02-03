618a2bc9aced04c41c0dd26cdfd465de146c835a
johnedisc <christopher.johnedis@gmail.com>
2023-02-03 12:01:44 -0800

    add test for calcAge method

--- __test__/planetAges.test.js
+++ __test__/planetAges.test.js
@@ -4,16 +4,23 @@ import { Person } from './../src/js/planetAges.js';
 describe('planetAges class', () => {
 
   let planetAges;
+  let person;
 
   beforeEach(() => {
     planetAges = new PlanetAges;
+    let person = new Person(56);
   });
 
   test('should return propery values when accessed', () => {
     expect(Object.keys(planetAges)).toEqual(['mercury','venus','mars','jupiter']);
   });
 
-
+  describe('calcAges', () => {
+    
+    test('should calculate and return converted ages for all properties of PlanetAges', () => {
+      expect(planetAges.calcAges(person.age)).toEqual([54,233.33,90.32,29.78,4.72)];
+    });
+  });
 });
 
 describe('Person class', () => {
