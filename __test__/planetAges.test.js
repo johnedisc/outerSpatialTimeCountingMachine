9a25f69f16173fea10ba41185a0a9089b692a027
johnedisc <christopher.johnedis@gmail.com>
2023-02-03 11:41:04 -0800

    change super class functionality and make person its own class

--- __test__/planetAges.test.js
+++ __test__/planetAges.test.js
@@ -13,24 +13,14 @@ describe('planetAges class', () => {
     expect(Object.keys(planetAges)).toEqual(['mercury','venus','mars','jupiter']);
   });
 
-  describe('Person class', () => {
-
-    let person;
-
-    beforeEach(() => {
-      person = new Person;
-    });
-
-    test('should return properties of planet ages via inheritance', () => {
-      expect(Object.keys(person)).toEqual(['mercury','venus','mars','jupiter']);
-    });
-
-    test('should reflect age argument passed into function', () => {
-      person = new Person(36);
-      expect(person.age).toEqual(36);
-    });
-
-  });
 
 });
 
+describe('Person class', () => {
+
+  test('should reflect age argument passed into function', () => {
+    let person = new Person(36);
+    expect(person.age).toEqual(36);
+  });
+
+});
