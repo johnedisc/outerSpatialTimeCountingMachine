c6aaf523ee98422c01bacf7edda6d02fc94e4649
johnedisc <christopher.johnedis@gmail.com>
2023-02-03 13:30:13 -0800

    sucessfully remove person object

--- __test__/planetAges.test.js
+++ __test__/planetAges.test.js
@@ -1,34 +1,26 @@
-import { PlanetAges, Person } from './../src/js/planetAges.js';
+import { PlanetAges } from './../src/js/planetAges.js';
 
 describe('planetAges class', () => {
 
   let planetAges;
-  let person;
 
   beforeEach(() => {
-    planetAges = new PlanetAges;
-    person = new Person(56);
+    planetAges = new PlanetAges(56);
   });
 
-  test('should return propery values when accessed', () => {
-    expect(Object.keys(planetAges)).toEqual(['mercury','venus','mars','jupiter']);
+  test('should return properties when accessed', () => {
+    expect(Object.keys(planetAges)).toEqual(['age','mercury','venus','mars','jupiter']);
+  });
+  
+  test('should reflect age argument passed into function', () => {
+    expect(planetAges.age).toEqual(56);
   });
 
   describe('calcAges() method', () => {
 
     test('should calculate and return converted age for all properties of PlanetAges', () => {
-      console.log(person);
-      expect(planetAges.calcAges(person.age)).toEqual([54,233.33,90.32,29.78,4.72]);
+      expect(planetAges.calcAges(this.age)).toEqual([54,233.33,90.32,29.78,4.72]);
     });
 
   });
-});
-
-describe('Person class', () => {
-
-  test('should reflect age argument passed into function', () => {
-    let person = new Person(36);
-    expect(person.age).toEqual(36);
-  });
-
 });
