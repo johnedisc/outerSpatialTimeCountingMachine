918c41ba9635be6be48ebd1c4d1a8e0872278e00
johnedisc <christopher.johnedis@gmail.com>
2023-02-03 10:54:41 -0800

    set up test to if person inherits from planetage

--- __test__/planetAges.test.js
+++ __test__/planetAges.test.js
@@ -11,22 +11,20 @@ describe('planetAges class', () => {
   test('should return propery values when accessed', () => {
     expect(Object.keys(planetAges)).toEqual(['mercury','venus','mars','jupiter']);
   });
+
+  describe('Person class', () => {
+
+    let person;
+
+    beforeEach(() => {
+      person = new Person;
+    });
+
+    test('should return properties of planet ages via inheritance', () => {
+      expect(Object.keys(person)).toEqual(['mercury','venus','mars','jupiter']);
+    });
+    
+  });
+  
 });
 
-//describe('entry', () => {
-//
-//  let reusableTest;
-//
-//  beforeEach(() => {
-//    reusableTest = new Entry('3.2');
-//  });
-//
-//  afterEach(() => {
-//    reusableTest = '';
-//  });
-//
-//  test('should correctly create a entry object with three lengths', () => {
-//    const entry = new entry('2.3');
-//    expect(entry.date).toEqual('2.3');
-//  });
-//});
